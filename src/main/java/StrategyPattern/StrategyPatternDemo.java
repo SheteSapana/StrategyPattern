56876383d1a146d650c95e465597710ad3be7364
SheteSapana <shetesapana@gmail.com>
2021-07-08 11:07:41 -0700

    StrategyPattern

---
+++ src/main/java/StrategyPattern/StrategyPatternDemo.java
@@ -0,0 +1,9 @@
+package StrategyPattern;
+
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+       }
+}
